487ea6c50aa040d50c14bdbbfe5aa439ee2bc81a
Roman Babiy <romchik.babiy@gmail.com>
2022-07-23 16:08:56 +0300

    add transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
 import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
 import user from "../data/user.json"
 import data from "../data/data.json"
 import friends from "../data/friends.json"
+import transactions from "../data/transactions.json"
 
 export const App = () => {
   return (
@@ -27,6 +29,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList items={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
